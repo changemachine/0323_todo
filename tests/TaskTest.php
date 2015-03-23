811394d67b44a6e4e76d218534ad4a01629e4167
james nielson & david eliason <student@epicodus.com>
2015-03-23 09:51:28 -0700

    first commit with structure of src and test files

---
+++ tests/TaskTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Task.php";
+    require_once "src/Category.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do_test');
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+            
+
+
+    }
+?>
